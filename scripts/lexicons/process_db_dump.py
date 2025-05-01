41e098ffd98eaa0b59e89acd5c8c8e9e89b062ef
Elen Kartina <affenmilchmann@gmail.com>
2025-05-01 14:21:47 +0300

    lexicons updated from new DB version

--- scripts/lexicons/process_db_dump.py
+++ scripts/lexicons/process_db_dump.py
@@ -1,14 +1,28 @@
 from pathlib import Path
+from typing import List, Tuple
+import re
 import csv
 import os
 
 input_dir = Path(__file__).parent.joinpath('db_dumps')
-output_dir = Path(__file__).parent.joinpath('lexd_lexicons')
+output_dir = Path(__file__).parent.parent.parent.joinpath('lexd/lexicons')
 
 comment_distance = 25
 
 fixes = {
-    'ӯ̊': 'ӯ'
+    'ӯ̊': 'ӯ', # invalid set of diacritics
+    'ҳ̌': 'ҳ',
+    'ц̌': 'ч',
+    '́': '' # remove stress
+}
+
+noun_tags = ['pl_all', 'sg']
+noun_plurals = {
+    'pl_in-laws': ['свояк', 'своячн', 'зять', 'невестка', 'тесть', 'деверь', 'шурин', 'теща', 'свекровь'],
+    'pl_cousins': ['двоюродный брат', 'двоюродная сестра'],
+    'pl_sisters': ['сестра', 'сестричка'],
+    'pl_timesOfDay': ['утро', 'утрен', 'день', 'дневн', 'вечер', 'ночь', 'ночн', 'сумерки', 'закат', 'восход'],
+    'pl_timesOfYear': ['весна', 'зима', 'лето', 'осень'],
 }
 
 def is_a_word(word: str) -> bool:
@@ -20,7 +34,21 @@ def fix(word: str) -> str:
         word = word.replace(bad, good)
     return word
 
-def process(file: Path, out_file) -> None:
+def file2lexicon(file: Path) -> str:
+    return 'Lexicon' + ''.join(x.capitalize() for x in file.stem.split('_'))
+
+def write_lexicon(file: Path, lines: List[Tuple[str, str]]):
+    if not file.exists():
+        with open(file, 'w', encoding='utf-8') as f:
+            if 'noun' in file.name:
+                f.write(f'LEXICON {file2lexicon(file)}[{",".join(noun_tags)}]\n')
+            else:
+                f.write(f'LEXICON {file2lexicon(file)}\n')
+    with open(file, 'a', encoding='utf-8') as f:
+        for w, com in lines:
+            f.write(f"{w}{' '*(comment_distance - len(w))}# {com}\n")    
+
+def process(file: Path) -> None:
     # reading lines in a list of tuples
     # and filtering out non-words
     lines = []
@@ -28,7 +56,7 @@ def process(file: Path, out_file) -> None:
         reader = csv.reader(f)
         _, _ = next(reader)
         lines = [(w.lower(), com) for w, com in reader if is_a_word(w)]
-        lines = [(fix(w), com) for w, com in lines]
+        lines = [(fix(w), com.replace('\n', '')) for w, com in lines]
 
     # sorting lines alphabetically
     lines.sort()
@@ -43,22 +71,43 @@ def process(file: Path, out_file) -> None:
             deleted.add(i)
     lines = [lines[i] for i in range(len(lines)) if not i in deleted]
     # adding gender tag if present
+    out_file = f'{file.stem}.lexd'
     allowed_tags = ['f', 'm']
     tag = ''
     for t in allowed_tags:
         if file.stem.endswith(f'_{t}'):
             tag = t
     if tag != '':
+        out_file = out_file.replace(f'_{tag}', '')
         for i in range(len(lines)):
             lines[i] = (f'{lines[i][0]}[{tag}]', lines[i][1])
+    # adding noun tags
+    if 'noun' in file.stem:
+        for i in range(len(lines)):
+            for tag, triggers in noun_plurals.items():
+                found = False
+                for trig in triggers:
+                    if trig in lines[i][1]:
+                        lines[i] = (f'{lines[i][0]}[{tag}]', lines[i][1])
+                        found = True
+                        break
+                if found: 
+                    break
+    # fixing inf stems
+    if 'verb_inf' in file.stem:
+        for i in range(len(lines)):
+            lines[i] = (lines[i][0].removesuffix('оw'), lines[i][1])
+                
     # writing words
-    with open(out_file, 'w', encoding='utf-8') as fo:
-        for w, com in lines:
-            fo.write(f"{w}{' '*(comment_distance - len(w))}# {com}\n")
+    write_lexicon(output_dir.joinpath(out_file), lines)
 
 if __name__ == '__main__':
+    for f in output_dir.iterdir():
+        if f.is_file():
+            f.unlink()
     csv_files = [input_dir.joinpath(f) 
                  for f in os.listdir(input_dir) 
                  if f.endswith('.csv')]
+    csv_files.sort()
     for f in csv_files:
-        process(f, output_dir.joinpath(f'{f.stem}.txt'))
+        process(f)
